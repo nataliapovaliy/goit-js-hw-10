68d0c34bf0c6cb1d3342f6bac195ea143ece47e9
nataliapovaliy <natalia.bg1125@gmail.com>
2022-12-25 22:13:33 +0200

    fetch + index bases

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const onInput = document.querySelector('#search-box');
+const cntrList = document.querySelector('.country-list');
+const cntrInfo = document.querySelector('.country-info');
+
+onInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(event) {
+    event.preventDefault();
+
+    let onSearch = onInput.value.trim();
+    console.log(onSearch);
+
+    fetchCountries(onSearch)
+        .then(data => {
+            if (data.length > 10) {
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+            }
+            if (data.length > 1 && data.length <= 10) {
+                Notiflix.Notify.success('Success');
+            }
+        })
+    
+        .catch(error => {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+        });
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,11 @@
+const searchParams = 'name.official, capital, population, flags, languages';
+
+export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=${searchParams}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+}
